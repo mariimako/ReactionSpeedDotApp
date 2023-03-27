a675dfb4ca91ca587ee78834eb08d112725d3747
mariimako <shepmar10@gmail.com>
2023-03-27 13:36:30 -0700

    initial GUI completion

---
+++ src/main/ui/drawer/GameDraw.java
@@ -0,0 +1,63 @@
+package ui.drawer;
+
+import model.*;
+
+import java.awt.*;
+
+public class GameDraw {
+
+    SGame playingGame;
+
+    private static final int PlAYER_SIZE_X = 30;
+    private static final int PLAYER_SIZE_Y = 30;
+
+    private static final int BULLET_SIZE_X = 5;
+    private static final int BULLET_SIZE_Y = 8;
+
+    private static final int ENEMY_SIZE_X = 30;
+    private static final int ENEMY_SIZE_Y = 50;
+
+
+    public GameDraw(SGame game) {
+        playingGame = game;
+    }
+
+    /*
+    EFFECTS: draws the game state
+    MODFIES: g
+    */
+    public void draw(Graphics g) {
+        for (Being sprite : playingGame.getBeings()) {
+            drawBeing(g, sprite);
+        }
+
+        Color saved = g.getColor();
+        g.setColor(new Color(0, 0, 0));
+        g.setFont(new Font("Arial", Font.BOLD, 20));
+        FontMetrics fm = g.getFontMetrics();
+        String str = "Current Enemies: " + playingGame.getEnemies().size();
+        g.drawString(str, SGame.WIDTH - fm.stringWidth(str), SGame.HEIGHT);
+        str = "Current Health: " + playingGame.getPlayer().getHealth();
+        g.drawString(str,  0, SGame.HEIGHT);
+        g.setColor(saved);
+
+    }
+
+
+    public void drawBeing(Graphics g, Being e) {
+        Color savedCol = g.getColor();
+        if (e instanceof Enemy) {
+            g.fillOval((int) e.getPosX() - ENEMY_SIZE_X / 2, (int) e.getPosY() - ENEMY_SIZE_Y / 2,
+                    ENEMY_SIZE_X, ENEMY_SIZE_Y);
+        } else if (e instanceof Bullet) {
+            g.fillOval((int) e.getPosX() - BULLET_SIZE_X / 2, (int) e.getPosY() - BULLET_SIZE_Y / 2,
+                    BULLET_SIZE_X, BULLET_SIZE_Y);
+        } else if (e instanceof Player) {
+            g.fill3DRect((int) e.getPosX() - PlAYER_SIZE_X / 2, (int) e.getPosY() - PLAYER_SIZE_Y / 2,
+                    PlAYER_SIZE_X, PLAYER_SIZE_Y, false);
+        }
+        g.setColor(e.getColor());
+        g.setColor(savedCol);
+    }
+
+}
